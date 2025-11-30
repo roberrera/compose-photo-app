99cde10815cc760ae7a47739c35cc74e392da493
roberrera <roberterrera@gmail.com>
2025-11-29 23:18:10 -0500

    Implement top bar and back button

--- app/src/main/java/com/roberrera/resytakehome/MainActivity.kt
+++ app/src/main/java/com/roberrera/resytakehome/MainActivity.kt
@@ -6,9 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.roberrera.resytakehome.ui.ResyTakeHomeNavigation
 import com.roberrera.resytakehome.ui.theme.ResyTakeHomeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,12 +30,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         enableEdgeToEdge()
         setContent {
             ResyTakeHomeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ResyTakeHomeNavigation(Modifier.padding(innerPadding))
+                val navController = rememberNavController()
+                val backStackEntry by navController.currentBackStackEntryAsState()
+                // Get the current route to determine if we should show the TopAppBar
+                val currentRoute = backStackEntry?.destination?.route
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                            CenterAlignedTopAppBar(
+                                title = {
+                                    Text(stringResource(R.string.app_name))
+                                },
+                                navigationIcon = {
+                                    // Show back button only on the details screen
+                                    if (currentRoute?.startsWith("photoDetails") == true) {
+                                    IconButton(onClick = { navController.navigateUp() }) {
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                            contentDescription = "Back"
+                                        )
+                                    }
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    ResyTakeHomeNavigation(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
--- app/src/main/java/com/roberrera/resytakehome/ui/ResyTakeHomeNavigation.kt
+++ app/src/main/java/com/roberrera/resytakehome/ui/ResyTakeHomeNavigation.kt
@@ -1,33 +1,33 @@
 package com.roberrera.resytakehome.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.roberrera.resytakehome.model.PhotosViewModel
 
 @Composable
-fun ResyTakeHomeNavigation(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
+fun ResyTakeHomeNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
     val photosViewModel: PhotosViewModel = hiltViewModel()
 
     NavHost(
         navController = navController,
         startDestination = "photosList",
         modifier = modifier
-        ) {
+    ) {
         composable(route = "photosList") {
             PhotosListScreen(
                 viewModel = photosViewModel,
                 onPhotoClick = { photo ->
                     navController.navigate(
-                        "photoDetails/${photo.width}/${photo.height}/${photo.id}/${photo
-                            .fileName}"
+                        "photoDetails/${photo.width}/${photo.height}/${photo.id}/${photo.fileName}"
                     )
                 }
             )
